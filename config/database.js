c0cbacccfec18da6a6dc331474ae3a5f88319eb8
Leo Constant <101950707+leonardconstant1995@users.noreply.github.com>
2022-05-31 02:35:01 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://leoconstant:leo@cluster0.ari5v8s.mongodb.net/storyboard?retryWrites=true&w=majority', 
+    'dbName': 'storyboard'
+};
